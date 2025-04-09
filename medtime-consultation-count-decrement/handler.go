bd6a5fb57a29c56be508093f928541f64d0c571b
ibrokhimov.fr <fr.ibrokhimov@gmail.com>
2025-04-09 10:05:49 +0500

    implement core Handle function logic for request processing

--- medtime-consultation-count-decrement/handler.go
+++ medtime-consultation-count-decrement/handler.go
@@ -199,3 +199,132 @@ func (f FunctionAssert) GetBenchmarkRequest() Asserts {
 		},
 	}
 }
+
+// func main() {
+// 	body := `
+// 	{
+// 		"data":{
+// 			"app_id":"P-JV2nVIRUtgyPO5xRNeYll2mT4F5QG4bS",
+// 			"object_data":{
+// 				"cleints_id":"93a01c6d-7f29-41b8-b706-5f3a59af84fa"
+// 			}
+// 		}
+// 	}
+// 	`
+// 	fmt.Println(Handle([]byte(body)))
+// }
+
+// Handle a serverless request
+func Handle(req []byte) string {
+	var (
+		response Response
+		request  NewRequestBody
+	)
+
+	// defer func() {
+	// 	responseByte, _ := json.Marshal(response)
+	// 	Send(string(responseByte))
+	// }()
+
+	err := json.Unmarshal(req, &request)
+	if err != nil {
+		response.Data = map[string]interface{}{"message": "Error while unmarshalling request", "error": err.Error()}
+		response.Status = "error"
+		responseByte, _ := json.Marshal(response)
+		return string(responseByte)
+	}
+
+	if request.Data.AppId == "" {
+		response.Data = map[string]interface{}{"message": "App id required"}
+		response.Status = "error"
+		responseByte, _ := json.Marshal(response)
+		return string(responseByte)
+	}
+
+	if request.Data.ObjectData["consultation_type"] != "бесплатная консультация" {
+		response.Data = map[string]interface{}{"message": "paid-consultation"}
+		response.Data = map[string]interface{}{}
+		response.Status = "done" //if all will be ok else "error"
+		responseByte, _ := json.Marshal(response)
+
+		return string(responseByte)
+	}
+
+	getObjectRequest := map[string]interface{}{
+		"cleints_id": cast.ToString(request.Data.ObjectData["cleints_id"]),
+		"status":     []string{"активный"},
+	}
+
+	res, response, err := GetListSlimObject(GetListFunctionRequest{
+		BaseUrl:     baseUrl,
+		TableSlug:   "subscription_report",
+		AppId:       request.Data.AppId,
+		Request:     getObjectRequest,
+		DisableFaas: true,
+	})
+	if err != nil {
+		response.Data = map[string]interface{}{"message": "Error while GetListSlimObject", "error": err.Error()}
+		response.Status = "error"
+		responseByte, _ := json.Marshal(response)
+		return string(responseByte)
+	}
+
+	if len(res.Data.Data.Response) > 0 {
+		var (
+			oldestTime time.Time
+			oldestData map[string]interface{}
+		)
+		for _, v := range res.Data.Data.Response {
+			if consultationCount, ok := v["consultation_count"].(float64); ok && consultationCount > 0 {
+				if endDateStr, ok := v["end_date"].(string); ok {
+					endDate, err := time.Parse(time.RFC3339, endDateStr)
+					if err != nil {
+						continue
+					}
+					if oldestTime.IsZero() || endDate.Before(oldestTime) {
+						oldestTime = endDate
+						oldestData = v
+					}
+				}
+			}
+		}
+		if oldestData == nil {
+			response.Data = map[string]interface{}{"message": "Consultation Not Found", "error": err}
+			response.Status = "error"
+			responseByte, _ := json.Marshal(response)
+			return string(responseByte)
+		}
+		updateRequest := Request{
+			Data: map[string]interface{}{
+				"guid":               oldestData["guid"],
+				"consultation_count": cast.ToInt(oldestData["consultation_count"]) - 1,
+			},
+		}
+		res2, response2, err2 := UpdateObject(FunctionRequest{
+			BaseUrl:     baseUrl,
+			TableSlug:   "subscription_report",
+			AppId:       request.Data.AppId,
+			Request:     updateRequest,
+			DisableFaas: true,
+		})
+		if err2 != nil {
+			response2.Data = map[string]interface{}{"message": "Error while UpdateObject", "error": err.Error()}
+			response2.Status = "error"
+			responseByte, _ := json.Marshal(response2)
+			return string(responseByte)
+		}
+		response2.Data = res2.Data.Data
+		response2.Data = map[string]interface{}{}
+		response2.Status = "done" //if all will be ok else "error"
+		responseByte, _ := json.Marshal(response2)
+
+		return string(responseByte)
+
+	} else {
+		response.Data = map[string]interface{}{"message": "free-consultation not found"}
+		response.Status = "error" //if all will be ok else "error"
+		responseByte, _ := json.Marshal(response)
+
+		return string(responseByte)
+	}
+}
